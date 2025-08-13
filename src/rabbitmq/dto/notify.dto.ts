c56dc0aa28508738fabc0d6afb752e8296b14f84
Jean Lima <jeansantoslima17@gmail.com>
2025-08-12 21:00:32 -0300

    feat: update NotifyDto to validate messageId as a UUID

--- src/rabbitmq/dto/notify.dto.ts
+++ src/rabbitmq/dto/notify.dto.ts
@@ -1,8 +1,8 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class NotifyDto {
     @IsNotEmpty({ message: 'messageId é obrigatório' })
-    @IsString({ message: 'messageId deve ser uma string' })
+    @IsUUID('4', { message: 'messageId deve ser um UUID válido' })
     messageId: string;
 
     @IsNotEmpty({ message: 'A mensagem é obrigatória' })
